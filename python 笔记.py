9c0f37b39b142d71da6897a43ba21e6c223f5b2b
银古 <50387225+cxs1427@users.noreply.github.com>
2022-01-06 17:53:33 +0800

    python 笔记

---
+++ python 笔记.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# Author: CXS
+# @Time: 2022/1/5 17:05
+
+"""
+random 的使用：
+
+    不重复选样：
+        random.sample([1,2,3,4,5], k=3)
+        Out[16]: [2, 3, 5]
+    重复选样：
+        random.choices([1,2,3,4,5], k=3)
+        Out[10]: [4, 3, 3]
+    打乱顺序（不返回结果）：
+        t = [1,2,3,4,5]
+        random.shuffle(t)
+
+
+css 选择器：
+
+
+
+"""
